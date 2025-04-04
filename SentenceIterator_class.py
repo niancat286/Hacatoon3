054ae895f1976b0e4f383f1b0906cbe272a4073b
Maksym Mokrytskyi <maks.mokrytskyi.1234@gmail.com>
2025-04-04 11:44:49 +0300

    SentenceIterator upd

---
+++ SentenceIterator_class.py
@@ -0,0 +1,25 @@
+class SentenceIterator:
+    def __init__(self, text):
+        sentences = text.replace('!', '.').replace('?', '.').split('.')
+        self.sentences = [s.strip() for s in sentences if s.strip()]
+        self.index = 0
+
+    def __next__(self):
+        if self.index >= len(self.sentences):
+            raise StopIteration
+        sentence = self.sentences[self.index]
+        self.index += 1
+        return sentence
+
+class Words:
+    def __init__(self, text):
+        self.sentence = text
+
+    def __iter__(self):
+        return SentenceIterator(self.sentence)
+
+if __name__ == '__main__':
+    text = "Це приклад тексту, який містить різні слова! А ще тут є короткі слова?"
+    words = Words(text)
+    for word in words:
+        print(word)
