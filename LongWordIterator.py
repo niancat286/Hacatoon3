c02a1775f13b722ab10a226f16d90c7d1685b6e5
YevhenPoshtak <yevhenposhtak@knu.ua>
2025-04-04 11:02:49 +0300

    added class WordIterator

--- LongWordIterator.py
+++ LongWordIterator.py
@@ -1,3 +1,23 @@
+class WordIterator:
+    def __init__(self, text):
+        self.text = text
+        self.chars = list(text)
+        self.position = 0
+        self.current_word = ""
+
+    def __next__(self):
+        punctuation = '.,!?;:-()[]{}"\' '
+        while self.position < len(self.chars) and self.chars[self.position] in punctuation:
+            self.position += 1
+        if self.position >= len(self.chars):
+            raise StopIteration
+
+        self.current_word = ""
+        while (self.position < len(self.chars) and self.chars[self.position] not in punctuation): self.current_word += self.chars[self.position]
+        self.position += 1
+        return self.current_word
+
+
 class WordStartsWithIterator:
     def __init__(self, text, letter):
         self.letter = letter.lower()
